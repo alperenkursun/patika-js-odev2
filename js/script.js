0d44394f26e6af568f77d1b77b1e9c4089bc07fd
alperenkursun <alperenkursun@outlook.com>
2024-03-21 21:31:53 +0300

    check function,localstorage,toast added

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
-var todoListData = [];
+var x = localStorage.getItem("todoListData")
+  ? localStorage.getItem("todoListData")
+  : [];
+var todoListData = JSON.parse(x);
 const todoList = document.getElementById("list");
 
 const showList = () => {
@@ -9,8 +12,8 @@ const showList = () => {
     <i class="fa-solid fa-xmark position-absolute delete_button" onclick="delElement(${index})"></i>
     ${item.todo}
     <i class="fa-solid fa-check text-white position-absolute check_icon ${
-      item.isActive ? "" : "d-none"
-    }"></i>
+      item.isActive ? "text-white" : "text-secondary"
+    }" onclick="cheElement(${index})"></i>
     </li>`;
     todoList.insertAdjacentHTML("beforeend", todoDOM);
   });
@@ -28,16 +31,40 @@ const handleValue = () => {
   value = taskInput.value;
 };
 
+const toastButton = document.getElementById("toastButton");
+
 const newElement = () => {
+  if (taskInput.value.trim() == "") {
+    let toast = document.getElementById("liveToast2");
+    let bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+  } else {
+    let toast = document.getElementById("liveToast1");
+    let bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+    cleanList();
+    let todo = { todo: value, isActive: false };
+    todoListData.push(todo);
+    localStorage.setItem("todoListData", JSON.stringify(todoListData));
+    showList();
+  }
   taskInput.value = "";
-  cleanList();
-  let todo = { todo: value, isActive: false };
-  todoListData.push(todo);
-  showList();
 };
 
 const delElement = (index) => {
   cleanList();
   todoListData.splice(index, 1);
+  localStorage.setItem("todoListData", JSON.stringify(todoListData));
+  showList();
+};
+
+const cheElement = (index) => {
+  cleanList();
+  if (todoListData[index].isActive == true) {
+    todoListData[index].isActive = false;
+  } else if (todoListData[index].isActive == false) {
+    todoListData[index].isActive = true;
+  }
+  localStorage.setItem("todoListData", JSON.stringify(todoListData));
   showList();
 };
